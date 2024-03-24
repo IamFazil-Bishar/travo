234c3ced309873ba8448e5665efdd7cd2846ee6e
Fazil Bishar <124660578+IamFazil-Bishar@users.noreply.github.com>
2024-03-24 08:07:32 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -54,7 +54,7 @@ const connect = async () => {
 
 // middleware
 app.use(express.json());
-app.use(cors(corsOptions));
+// app.use(cors(corsOptions));
 app.use(cookieParser());
 app.use("/api/v1/auth", authRoute);
 app.use("/api/v1/tours", tourRoute);
