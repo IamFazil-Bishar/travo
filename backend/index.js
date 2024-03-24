13291500b258dd03ce28b0c05469728cf2c32931
Fazil Bishar <124660578+IamFazil-Bishar@users.noreply.github.com>
2024-03-24 08:10:50 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -24,19 +24,10 @@ const port = process.env.PORT || 8000;
 
  // Enable CORS middleware
 
-app.use((req, res, next) => {
-  const allowedOrigins = ["https://travo-2pk9.vercel.app"];
-  const origin = req.headers.origin;
-
-  if (allowedOrigins.includes(origin)) {
-    res.setHeader("Access-Control-Allow-Origin", origin);
-  }
-  
-  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
-  res.setHeader("Access-Control-Allow-Credentials", "true");
-  next();
-});
+app.use(cors({
+  origin: 'https://travo-2pk9.vercel.app',
+  credentials: true // Allow credentials to be sent
+}));
 
 // database connection
 mongoose.set("strictQuery", false);
